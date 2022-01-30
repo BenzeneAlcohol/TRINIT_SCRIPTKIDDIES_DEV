ef8d2b838843526018a72f33e77e341f534684d8
Utkarsh M <utkarshrm568@gmail.com>
2022-01-30 07:06:41 +0530

    isExpert Middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,5 @@
+const Team = require('./models/team');
+
 module.exports.isLoggedIn = function (req, res, next) {
   req.session.returnTo = req.originalUrl;
   if (!req.isAuthenticated()) {
@@ -5,4 +7,17 @@ module.exports.isLoggedIn = function (req, res, next) {
     return res.redirect('/login');
   }
   next();
+};
+
+module.exports.isExpert = async function (req, res, next) {
+  const team = await Team.findById(req.params.id).populate('members.user');
+
+  const members = team.members.filter(
+    (x) => x.user.username == req.user.username,
+  );
+  if (members.length > 0 && members[0].role === 'Expert') next();
+  else {
+    req.flash('error', "You Don't have that permission.");
+    res.redirect(`/teams`);
+  }
 };
--- src/routes/teams.js
+++ src/routes/teams.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const teams = require('../controllers/teams');
-const { isLoggedIn } = require('../middleware');
+const { isLoggedIn, isExpert } = require('../middleware');
 
 router
   .route('/')
-  .get(catchAsync(teams.index))
+  .get(isLoggedIn, catchAsync(teams.index))
   .post(isLoggedIn, catchAsync(teams.createTeam));
 
 router.get('/new', isLoggedIn, catchAsync(teams.renderNewForm));
@@ -14,11 +14,15 @@ router.get('/new', isLoggedIn, catchAsync(teams.renderNewForm));
 router
   .route('/:id')
   .get(catchAsync(teams.showTeam))
-  .put(isLoggedIn, catchAsync(teams.editTeam))
-  .delete(catchAsync(teams.deleteTeam));
+  .put(isLoggedIn, catchAsync(isExpert), catchAsync(teams.editTeam))
+  .delete(isLoggedIn, catchAsync(isExpert), catchAsync(teams.deleteTeam));
 
-router.route('/:id/edit').get(isLoggedIn, catchAsync(teams.renderEditForm));
+router
+  .route('/:id/edit')
+  .get(isLoggedIn, catchAsync(isExpert), catchAsync(teams.renderEditForm));
 
-router.route('/:id/members').post(isLoggedIn, catchAsync(teams.addMember));
+router
+  .route('/:id/members')
+  .post(isLoggedIn, catchAsync(isExpert), catchAsync(teams.addMember));
 
 module.exports = router;
